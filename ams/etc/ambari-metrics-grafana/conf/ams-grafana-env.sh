2f635a50a125c5803430bea1ae93cbca983bab34
root <root@node01.openstacklocal>
2018-04-25 22:00:44 +0000

    grafana config

---
+++ ams/etc/ambari-metrics-grafana/conf/ams-grafana-env.sh
@@ -0,0 +1,14 @@
+
+# Set environment variables here.
+
+# AMS UI Server Home Dir
+export AMS_GRAFANA_HOME_DIR=/usr/lib/ambari-metrics-grafana
+
+# AMS UI Server Data Dir
+export AMS_GRAFANA_DATA_DIR=/var/lib/ambari-metrics-grafana
+
+# AMS UI Server Log Dir
+export AMS_GRAFANA_LOG_DIR=/var/log/ambari-metrics-grafana
+
+# AMS UI Server PID Dir
+export AMS_GRAFANA_PID_DIR=/var/run/ambari-metrics-grafana
